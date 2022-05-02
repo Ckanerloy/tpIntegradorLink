8f4c019e039e91cd144c91c8b749dc04d9830ee2
Ckanerloy <camilakanerloy@gmail.com>
2022-05-02 13:05:07 -0300

    Proyecto Spring y gitignore

---
+++ src/main/java/ar/edu/utn/frba/tpIntegrador/TpIntegradorApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.tpIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ar/edu/utn/frba/tpIntegrador/model/Usuario.java
@@ -0,0 +1,6 @@
+package ar.edu.utn.frba.tpIntegrador.model;
+
+public class Usuario {
+	private String user;
+	private String contra;
+}
