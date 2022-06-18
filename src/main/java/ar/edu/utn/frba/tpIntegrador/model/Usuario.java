d7d664021d9b020e71152ec57bed2832a201512f
Ckanerloy <camilakanerloy@gmail.com>
2022-06-17 21:54:23 -0300

    UUID para el id del usuario funcionando

--- src/main/java/ar/edu/utn/frba/tpIntegrador/model/Usuario.java
+++ src/main/java/ar/edu/utn/frba/tpIntegrador/model/Usuario.java
@@ -1,15 +1,23 @@
 package ar.edu.utn.frba.tpIntegrador.model;
 
+import java.util.UUID;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+import org.hibernate.annotations.GenericGenerator;
+
 @Entity
 public class Usuario{
-	@Id @GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer id;
+	@Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false, unique=true, columnDefinition = "BINARY(16)")
+    private UUID id;
 	private String user;
 	private String contra;
 	@OneToOne
@@ -26,12 +34,6 @@ public class Usuario{
 		super();
 	}
 	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
 	public String getUser() {
 		return user;
 	}
--- src/main/java/ar/edu/utn/frba/tpIntegrador/repo/RepoUsuario.java
+++ src/main/java/ar/edu/utn/frba/tpIntegrador/repo/RepoUsuario.java
@@ -1,11 +1,13 @@
 package ar.edu.utn.frba.tpIntegrador.repo;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import ar.edu.utn.frba.tpIntegrador.model.Usuario;
 
 @RepositoryRestResource(path="usuarios")
-public interface RepoUsuario extends PagingAndSortingRepository<Usuario, Integer> {
+public interface RepoUsuario extends PagingAndSortingRepository<Usuario, UUID> {
 
 }
