456300805961413d5bac069e8df953a4ffba50ff
Ckanerloy <camilakanerloy@gmail.com>
2022-06-24 06:27:59 -0300

    Terminado

--- src/main/java/ar/edu/utn/frba/tpIntegrador/repo/VendedorController.java
+++ src/main/java/ar/edu/utn/frba/tpIntegrador/repo/VendedorController.java
@@ -1,5 +1,18 @@
 package ar.edu.utn.frba.tpIntegrador.repo;
 
+import java.awt.print.Pageable;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import ar.edu.utn.frba.tpIntegrador.model.Vendedor;
+
 public class VendedorController {
 
 }
